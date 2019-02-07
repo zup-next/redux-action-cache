7c7204836495d5e9119815b48082447d6bcc7289
Isac <isac_asj91@hotmail.com>
2019-02-07 11:56:37 -0200

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import babel from 'rollup-plugin-babel'
+import { uglify } from 'rollup-plugin-uglify'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'lib/redux-action-cache.js',
+    format: 'cjs',
+  },
+  watch: {
+    include: 'src/**',
+    exclude: 'node_modules/**',
+  },
+  plugins: [
+    babel({
+      babelrc: true,
+      exclude: 'node_modules/**',
+    }),
+    uglify(),
+  ],
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+function sayHello() {
+  return 'hello'
+}
+
+export default sayHello
