2004cc98ca9815f3d89c195bd08477f4a70f4420
Isac <isac_asj91@hotmail.com>
2019-03-08 13:50:44 -0300

    update dependencies and config comma eslint added

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
     "extends": "standard",
     "env": {
         "jest": true
-      }  
+      },
+      "rules": {
+        "comma-dangle": ["error", "always-multiline"]
+      }, 
 };
