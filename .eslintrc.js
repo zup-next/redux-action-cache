9b1fecfafc82fd54dd3c7964e50ee40ae1f43fd4
Isac <isac_asj91@hotmail.com>
2019-03-12 15:43:39 -0300

    add some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,9 @@ module.exports = {
       "jest": true
     },
     "rules": {
-      "comma-dangle": ["error", "always-multiline"]
+      "comma-dangle": ["error", "always-multiline"],
+      "@typescript-eslint/type-annotation-spacing": ["error", { "before": false, "after": true, overrides: { arrow: { before: true, after: true }} }],
+      "@typescript-eslint/ban-types": "error",
+      "@typescript-eslint/prefer-interface": "error"
     },
 };
