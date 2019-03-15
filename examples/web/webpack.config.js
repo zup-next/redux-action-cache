7ca06a7893f2860d1444d5c4178a1fdaf8f93c91
Raphael de Souza Pimenta <raphael@Nextel-Raphael.local>
2019-03-15 10:16:24 -0300

    Project Example Structure

---
+++ examples/web/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+
+  devServer: {
+    historyApiFallback: true,
+  },
+
+  mode: 'development',
+
+  entry: {
+    main: './src/index.tsx',
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    publicPath: '/',
+  },
+
+  resolve: {
+    alias: {
+      containers: path.resolve(__dirname, 'src/containers'),
+    },
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/,
+      }, {
+        test: /.js$/,
+        loader: 'source-map-loader',
+        enforce: 'pre',
+      },
+    ],
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: 'vendor',
+          chunks: 'initial',
+          enforce: true,
+        },
+      },
+    },
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Redux Action Cache - Web Example',
+      template: './src/index.html',
+    }),
+  ],
+
+}
