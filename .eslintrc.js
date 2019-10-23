75715b3ee5037260a8dd33a0d79218707fa76a3e
Isac Junior <isac_asj91@hotmail.com>
2019-10-23 11:13:58 -0300

    removed rule eslint interface to fix error

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,12 @@ module.exports = {
   "parser": "@typescript-eslint/parser",
   "plugins": ["@typescript-eslint"],
   "env": {
-      "jest": true
-    },
-    "rules": {
-      "comma-dangle": ["error", "always-multiline"],
-      "@typescript-eslint/type-annotation-spacing": ["error", { "before": false, "after": true, overrides: { arrow: { before: true, after: true }} }],
-      "@typescript-eslint/ban-types": "error",
-      "@typescript-eslint/prefer-interface": "error",
-      "@typescript-eslint/no-unused-vars": "error"
-    },
+    "jest": true
+  },
+  "rules": {
+    "comma-dangle": ["error", "always-multiline"],
+    "@typescript-eslint/type-annotation-spacing": ["error", { "before": false, "after": true, overrides: { arrow: { before: true, after: true } } }],
+    "@typescript-eslint/ban-types": "error",
+    "@typescript-eslint/no-unused-vars": "error"
+  },
 };
